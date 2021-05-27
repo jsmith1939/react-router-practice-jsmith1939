3ddb2cd9c96491d39a9fe693865682455c8cba9a
J Parrott <j@Js-MacBook-Pro.attlocal.net>
2021-05-27 05:42:57 -0400

    Connected

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,9 +3,9 @@ import React from "react";
 function Welcome(props) {
   return (
     <span>
-      Hello _name_!
+      Hello _name_! 
     </span>
   );
 }
 
-export default Product;
+export default Welcome;
